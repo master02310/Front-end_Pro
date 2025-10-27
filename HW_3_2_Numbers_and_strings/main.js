32a7cbd3f8331506be0dcc6ece24902da37202e6
master02310 <danilmaster02@gmail.com>
2025-10-27 15:24:46 +0200

    HW 3.2

---
+++ HW_3_2_Numbers_and_strings/main.js
@@ -0,0 +1,7 @@
+let inputOne = prompt("What is the first number?");
+let inputTwo = prompt("What is the second number?");
+let inputThree = prompt("What is the third number?");
+
+const template = `First input "${inputOne}", second input "${inputTwo}", third input "${inputThree}"`;
+
+console.log(template);

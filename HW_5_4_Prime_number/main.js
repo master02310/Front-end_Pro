694b99af3e68b79b1ba907047794c447a9d5ff7a
master02310 <danilmaster02@gmail.com>
2025-11-04 17:10:49 +0200

    HW 5.4

---
+++ HW_5_4_Prime_number/main.js
@@ -0,0 +1,9 @@
+let promptNum = prompt("Enter your number");
+
+if (promptNum % 2 === 0) {
+    console.log("Unfortunately, your number is not prime.")
+} else if (promptNum % 3 === 0) {
+    console.log("Unfortunately, your number is not prime.")
+} else {
+    console.log("Your number is prime.");
+}
